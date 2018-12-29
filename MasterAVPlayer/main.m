24a893d6546f01a38c49c4d7003b07a25cd3df2c
HeDaShuang <874480304@qq.com>
2018-12-29 17:25:01 +0800

    Initial Commit

---
+++ MasterAVPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MasterAVPlayer
+//
+//  Created by eric on 2018/12/29.
+//  Copyright © 2018 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

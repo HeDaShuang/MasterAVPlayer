81b4fcd68fe27dd297e1e8d8874faffe7d1b584a
HeDaShuang <874480304@qq.com>
2019-01-16 11:26:31 +0800

    创建UIView的category文件用于获取frame属性

--- MasterAVPlayer/UIView+FrameAccessor.h
+++ MasterAVPlayer/UIView+FrameAccessor.h
@@ -1,7 +1,7 @@
 //
 //  UIView+FrameAccessor.h
 //  MasterAVPlayer
-//
+//  获取UIView的frame相关属性
 //  Created by eric on 2019/1/16.
 //  Copyright © 2019 eric. All rights reserved.
 //
@@ -11,6 +11,40 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView (FrameAccessor)
+- (CGPoint)origin;
+- (void)setOrigin:(CGPoint)newOrigin;
+
+- (CGSize)size;
+- (void)setSize:(CGSize)newSize;
+
+- (CGFloat)x;
+- (void)setX:(CGFloat)newX;
+
+- (CGFloat)y;
+- (void)setY:(CGFloat)newY;
+
+- (CGFloat)height;
+- (void)setHeight:(CGFloat)newHeight;
+
+- (CGFloat)width;
+- (void)setWidth:(CGFloat)newWidth;
+
+
+- (CGFloat)maxX;
+- (void)setMaxX:(CGFloat)newMaxX;
+
+- (CGFloat)midX;
+- (void)setMidX:(CGFloat)newMidX;
+
+- (CGFloat)maxY;
+- (void)setMaxY:(CGFloat)newMaxY;
+
+- (CGFloat)midY;
+- (void)setMidY:(CGFloat)newMidY;
+
+@property (nonatomic,assign) CGFloat right;
+
+@property (nonatomic,assign) CGFloat bottom;
 
 @end
 

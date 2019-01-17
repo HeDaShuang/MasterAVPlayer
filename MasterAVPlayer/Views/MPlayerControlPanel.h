9d7305feb967f4edc68a62482465b49635f77a2e
HeDaShuang <874480304@qq.com>
2019-01-17 16:34:50 +0800

    播放器控制面板

--- MasterAVPlayer/Views/MPlayerControlPanel.h
+++ MasterAVPlayer/Views/MPlayerControlPanel.h
@@ -1,7 +1,7 @@
 //
 //  MPlayerControlPanel.h
 //  MasterAVPlayer
-//
+//  播放器控制面板
 //  Created by eric on 2019/1/16.
 //  Copyright © 2019 eric. All rights reserved.
 //
@@ -11,6 +11,31 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MPlayerControlPanel : UIView
+
+//视频封面
+@property(nonatomic, strong) UIImageView *courseCover;
+
+//全屏返回按钮(退出全屏)
+@property(nonatomic, strong) UIButton *quitFScreenBtn;
+
+//开始播放/暂停播放按钮
+@property(nonatomic, strong) UIButton *playBtn;
+
+//上一集按钮
+@property(nonatomic, strong) UIButton *previousBtn;
+
+//下一集按钮
+@property(nonatomic, strong) UIButton *nextBtn;
+
+//加载中指示器
+@property(nonatomic, strong) UIImageView *loadingIV;
+
+//加载中和加载失败提示
+@property(nonatomic, strong) UILabel *reloadLabel;
+
+//获取底部操控视图的滑竿
+@property(nonatomic, strong) UISlider *pSlider;
+
 
 @end
 
--- MasterAVPlayer/Views/MPlayerControlPanel.m
+++ MasterAVPlayer/Views/MPlayerControlPanel.m
@@ -10,12 +10,18 @@
 
 @implementation MPlayerControlPanel
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor purpleColor];
+    }
+    
+    return self;
 }
-*/
+
+-(void)setControlPanelUISelector{
+    
+}
+
 
 @end
